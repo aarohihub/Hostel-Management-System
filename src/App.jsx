58d7824d0f57c9f9129487d36f49354b25a7748c
riketpokharel <pokharelriket777@gmail.com>
2024-04-21 20:22:35 +0545

    loginsignup

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./Components/Navbar";
-import { Home, About, Facilities, Rooms, Gallery, Contacts, Login, Signup } from "./pages";
+import {
+  Home,
+  About,
+  Facilities,
+  Rooms,
+  Gallery,
+  Contacts,
+  Login,
+  Signup,
+  Dashboard,
+} from "./pages";
 
 import Footer from "./pages/Footer";
 
-
-function App(){
-  return <div className="App">
-    
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/facilities" element={<Facilities />}/>
-      <Route path="/rooms" element={<Rooms />}/>
-      <Route path="/gallery" element={<Gallery />}/>
-      <Route path="/contacts" element={<Contacts />}/>
-      <Route path="/login" element={<Login />}/>
-      <Route path="/signup" element={<Signup />}/>
-    </Routes>
-    <Footer />
-    
-  </div>;
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/facilities" element={<Facilities />} />
+        <Route path="/rooms" element={<Rooms />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
